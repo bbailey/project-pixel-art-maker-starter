6c193aa960712c5bfe02089088bd648e9cbf5af5
Bobby Bailey <nosanity79@gmail.com>
2018-04-08 16:17:49 -0400

    implement core functionality
    Core functionality of the pixel art maker is implemented. Users can enter a grid size and create a new grid (which destroys any existing grid), and then select a colour and click table cells to change their colour.

--- designs.js
+++ designs.js
@@ -3,8 +3,25 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(e) {
+    e.preventDefault();
 
-// Your code goes here!
+    grid = $('#pixelCanvas').find('*');
+    grid.each(function() {
+        $(this).remove();
+    });
+    let h = $("#inputHeight").val();
+    let w = $("#inputWidth").val();
 
+    for(let x = 0; x < h; x++) {
+      $('#pixelCanvas').append("<tr>");
+      for(let y = 0; y < w; y++) {
+        $('tr:last-child').append("<td>");
+      }
+    }
 }
+
+$('#sizePicker').submit(makeGrid);
+$('#pixelCanvas').on('click','td', function(e){
+  $(e.target).css('background-color',$('#colorPicker').val());
+})
